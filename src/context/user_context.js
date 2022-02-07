81402bcd9f08fabc75b2f8963606ab54b1831749
IslamHegazy7196 <islamfathihegazy04@gmai.com>
2022-02-07 16:32:30 +0200

    fix user context comment bug

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -1,4 +1,4 @@
-// import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 // import { useAuth0 } from '@auth0/auth0-react'
 
 const UserContext = React.createContext()
